e7f7b97cbbfd32198f56c9fdf2ed4f0a5c94eed8
ihni7 <ihnishat1998@gmail.com>
2020-09-06 09:27:06 +0600

    ADDED: No data screen

--- lib/screens/note_screen.dart
+++ lib/screens/note_screen.dart
@@ -112,19 +112,47 @@ class NoteScreen extends StatelessWidget {
                 stream: database.watchAllNotes(),
                 builder: (context, snapshot) {
                   final notes = snapshot.data ?? List();
-                  return GridView.count(
-                    physics: BouncingScrollPhysics(),
-                    shrinkWrap: true,
-                    crossAxisCount: 2,
-                    children: List.generate(
-                      notes.length,
-                      (index) => Padding(
-                        padding: EdgeInsets.only(
-                            right: index.isEven ? 10 : 0, bottom: 10),
-                        child: NoteCard(note: notes[index]),
-                      ),
-                    ),
-                  );
+                  return snapshot.data.length > 0
+                      ? GridView.count(
+                          physics: BouncingScrollPhysics(),
+                          shrinkWrap: true,
+                          crossAxisCount: 2,
+                          children: List.generate(
+                            notes.length,
+                            (index) => Padding(
+                              padding: EdgeInsets.only(
+                                  right: index.isEven ? 10 : 0, bottom: 10),
+                              child: NoteCard(note: notes[index]),
+                            ),
+                          ),
+                        )
+                      : SizedBox(
+                          height: MediaQuery.of(context).size.height / 2,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              Text(
+                                '\"If you give people nothingness, they can ponder what can be achieved from that nothingness\"',
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontStyle: FontStyle.italic,
+                                  fontSize: 18,
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(right: 25.0),
+                                child: Text(
+                                  '-Tadao Ando',
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
                 },
               ),
             ),
